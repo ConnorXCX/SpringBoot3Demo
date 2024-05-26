97932d547222693bb4cc3d97347eb00e8186422d
Connor Thomas <ConnorNoelThomas@gmail.com>
2024-05-26 02:50:52 -0400

    Add H2 in-memory database with table schema and JSON data loader for Module 3.

--- runnerz/src/main/java/xcx/connor/runnerz/RunnerzApplication.java
+++ runnerz/src/main/java/xcx/connor/runnerz/RunnerzApplication.java
@@ -1,18 +1,9 @@
 package xcx.connor.runnerz;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import xcx.connor.runnerz.run.Location;
-import xcx.connor.runnerz.run.Run;
-import xcx.connor.runnerz.run.RunRepository;
 
 @SpringBootApplication
 public class RunnerzApplication {
