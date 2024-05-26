11903396c55ab21f2833f5a8e431cdbcbd79b856
Connor Thomas <ConnorNoelThomas@gmail.com>
2024-05-26 02:50:14 -0400

    Add H2 in-memory database with table schema and JSON data loader for Module 3.

--- runnerz/src/main/java/xcx/connor/runnerz/RunnerzApplication.java
+++ runnerz/src/main/java/xcx/connor/runnerz/RunnerzApplication.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 
 import xcx.connor.runnerz.run.Location;
 import xcx.connor.runnerz.run.Run;
+import xcx.connor.runnerz.run.RunRepository;
 
 @SpringBootApplication
 public class RunnerzApplication {
@@ -22,13 +23,14 @@ public class RunnerzApplication {
 		SpringApplication.run(RunnerzApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner runner() {
-		return args -> {
-			Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5,
-					Location.OUTDOOR);
-			log.info("Run: " + run);
-		};
-	}
+	// @Bean
+	// CommandLineRunner runner(RunRepository runRepository) {
+	// return args -> {
+	// Run run = new Run(1, "First Run", LocalDateTime.now(),
+	// LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5,
+	// Location.OUTDOOR);
+	// runRepository.create(run);
+	// };
+	// }
 
 }
---
+++ runnerz/src/main/java/xcx/connor/runnerz/run/Runs.java
@@ -0,0 +1,7 @@
+package xcx.connor.runnerz.run;
+
+import java.util.List;
+
+public record Runs(List<Run> runs) {
+
+}
