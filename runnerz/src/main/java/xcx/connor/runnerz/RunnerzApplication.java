290f522f3a1fc473d8add47d4938c16c293dde72
Connor Thomas <ConnorNoelThomas@gmail.com>
2024-05-26 05:12:56 -0400

    Add HTTP Client for Module 4.

--- runnerz/src/main/java/xcx/connor/runnerz/RunnerzApplication.java
+++ runnerz/src/main/java/xcx/connor/runnerz/RunnerzApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import xcx.connor.runnerz.user.User;
+import xcx.connor.runnerz.user.UserHttpClient;
 import xcx.connor.runnerz.user.UserRestClient;
 
 @SpringBootApplication
@@ -32,7 +36,15 @@ public class RunnerzApplication {
 	// }
 
 	@Bean
-	CommandLineRunner runner(UserRestClient client) {
+	UserHttpClient userHttpClient() {
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient))
+				.build();
+		return factory.createClient(UserHttpClient.class);
+	}
+
+	@Bean
+	CommandLineRunner runner(UserHttpClient client) {
 		return args -> {
 			List<User> users = client.findAll();
 			System.out.println(users);
