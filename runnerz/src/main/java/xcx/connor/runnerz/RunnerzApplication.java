d4eb1414a3f64542724f1a3286b2723cf3071ed7
Connor Thomas <ConnorNoelThomas@gmail.com>
2024-05-25 21:27:33 -0400

    Spring Boot starter files.

--- runnerz/src/main/java/xcx/connor/runnerz/RunnerzApplication.java
+++ runnerz/src/main/java/xcx/connor/runnerz/RunnerzApplication.java
@@ -3,11 +3,17 @@ package xcx.connor.runnerz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import foo.bar.WelcomeMessage;
+
 @SpringBootApplication
 public class RunnerzApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerzApplication.class, args);
+
+		var welcomeMessage = new WelcomeMessage();
+		System.out.println(welcomeMessage.getWelcomeMessage());
+
 	}
 
 }
