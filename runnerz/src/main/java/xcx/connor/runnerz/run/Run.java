e596ee2003354c0f8dfbc55b4ea5f858703ad3e5
Connor Thomas <ConnorNoelThomas@gmail.com>
2024-05-26 01:17:38 -0400

    Module 2 Complete.

--- runnerz/src/main/java/xcx/connor/runnerz/run/Run.java
+++ runnerz/src/main/java/xcx/connor/runnerz/run/Run.java
@@ -2,7 +2,21 @@ package xcx.connor.runnerz.run;
 
 import java.time.LocalDateTime;
 
-public record Run(Integer id, String title, LocalDateTime startedOn, LocalDateTime completedOn, Integer miles,
-        Location location) {
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
+public record Run(
+                Integer id,
+                @NotEmpty String title,
+                LocalDateTime startedOn,
+                LocalDateTime completedOn,
+                @Positive Integer miles,
+                Location location) {
+
+        public Run {
+                if (!completedOn.isAfter(startedOn)) {
+                        throw new IllegalArgumentException("completedOn must be after startedOn.");
+                }
+        }
 
 }
