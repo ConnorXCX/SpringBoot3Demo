ec424b1c3da60f0ac26994ec9be1d36b01a0addc
Connor Thomas <ConnorNoelThomas@gmail.com>
2024-05-26 03:51:19 -0400

    Add PostgreSQL Docker compose file and configuration for Module 3.

--- runnerz/src/main/java/xcx/connor/runnerz/run/Run.java
+++ runnerz/src/main/java/xcx/connor/runnerz/run/Run.java
@@ -2,16 +2,16 @@ package xcx.connor.runnerz.run;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 
-public record Run(
-                Integer id,
-                @NotEmpty String title,
-                LocalDateTime startedOn,
-                LocalDateTime completedOn,
+public record Run(@Id Integer id, @NotEmpty String title, LocalDateTime startedOn, LocalDateTime completedOn,
                 @Positive Integer miles,
-                Location location) {
+                Location location,
+                @Version Integer version) {
 
         public Run {
                 if (!completedOn.isAfter(startedOn)) {
